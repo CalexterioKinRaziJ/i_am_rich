9e26a9b9f75f0d9ab01187443f87a95608bf7238
Calexterio, Kin Razi J <72869071+CalexterioKinRaziJ@users.noreply.github.com>
2020-11-08 00:37:25 +0800

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text('I am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image:
+                AssetImage('images/diamond.png')
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.orange,
+          child: Icon(Icons.message)
+        ),
+      ),
+    ),
+  );
+}
